e17f3178d5158f17d4d71a60f1c0a40abacffe3c
LuchoProgramer <luchoviteri1990@gmail.com>
2024-12-02 11:40:30 -0500

    blogdetail

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -73,7 +73,7 @@ const BlogDetail = async ({ params }: BlogDetailPageProps) => {
     }
 
     return (
-        <div className="max-w-3xl mx-auto p-4 mt-12 mb-8">
+        <div className="max-w-3xl mx-auto p-10 mt-12 mb-8">
             <h2 className="text-3xl font-bold mb-6 text-center text-gray-800 dark:text-gray-200">
                 {blog.title}
             </h2>
